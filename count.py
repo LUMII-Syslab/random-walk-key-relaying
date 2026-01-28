08aebf104b92e9ec52a5a5436a147c3352985905
KrisjanisP <krisjanispetrucena@gmail.com>
2026-01-28 15:15:21 +0200

    print degrees of all vertices

--- count.py
+++ count.py
@@ -21,12 +21,14 @@ with open(nodes_file, 'r') as file:
 # read edges
 edge_count = 0
 adj = defaultdict[str, list[str]](list)
+self_loops = False
 with open(edges_file, 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         src, tgt = row['Source'], row['Target']
         if src == tgt:
             print(f"found self-loop {src}")
+            self_loops = True
             continue
         if src not in nodes:
             print(f"node {src} not in nodes file")
@@ -35,13 +37,23 @@ with open(edges_file, 'r') as file:
         adj[src].append(tgt)
         adj[tgt].append(src)
         edge_count += 1
+if not self_loops:
+    print(f"no self-loops")
 
+directed = False
+duplicate_edges = False
 for node in adj:
     for neighbor in adj[node]:
         if node not in adj[neighbor]:
             print(f"found directed edge {node} -> {neighbor}")
+            directed = True
         if adj[node].count(neighbor) > 1:
             print(f"found duplicate edge {node} -> {neighbor}")
+            duplicate_edges = True
+if not directed:
+    print(f"graph is undirected")
+if not duplicate_edges:
+    print(f"no duplicate edges")
 
 # compute degrees
 degrees = {node: len(adj[node]) for node in adj}
@@ -51,5 +63,9 @@ degree_1 = sorted([n for n, d in degrees.items() if d == 1])
 print(f"vertices: {len(nodes)}, edges: {edge_count}")
 if isolated:
     print(f"isolated nodes (degree 0): {sorted(isolated)}")
-if degree_1:
-    print(f"degree 1 nodes: {degree_1}")
+else:
+    print(f"no isolated nodes")
+
+# print degrees
+for (node, degree) in degrees.items():
+    print(f"{node}: {degree}")
