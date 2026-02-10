f44c6334c68b53b413967ee99477fdbecd2a318d
KrisjanisP <krisjanispetrucena@gmail.com>
2026-02-10 16:23:54 +0200

    add mean hops to pairs.csv

--- merge.py
+++ merge.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-GRAPH = "nsfnet"
+GRAPH = "secoqc"
 # files = [f"out2/{GRAPH}_{walk}_hops.csv" for walk in ["r", "nb", "lrv"]]
 # l = pl.read_csv(files[0])
 # for r_file in files[1:]:
@@ -8,10 +8,10 @@ GRAPH = "nsfnet"
 #     l = l.join(r, on=["source", "target"], how="inner", validate="1:1")
 #     assert l.shape[0] == r.shape[0]
 # l.write_csv(f"data/{GRAPH}/hops.csv")
-l_path  = pl.read_csv(f"data/{GRAPH}/pairs.csv")
-r_path = pl.read_csv(f"data/{GRAPH}/hops.csv")
-l = pl.read_csv(l_path)
-r = pl.read_csv(r_path).select(["source", "target", "lrv_mean_hops"])
+l = pl.read_csv(f"data/{GRAPH}/pairs.csv")
+r = pl.read_csv(f"data/{GRAPH}/hops.csv")
+r = r.select(["source", "target", "lrv_mean_hops"])
+r = r.rename({"lrv_mean_hops": "lrv_hops"})
 l = l.join(r, on=["source", "target"], how="inner", validate="1:1")
 assert l.shape[0] == r.shape[0]
 l.write_csv(f"data/{GRAPH}/pairs.csv")
