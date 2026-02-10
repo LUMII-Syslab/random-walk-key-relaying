a4362b89746acba949ef33b493406ff68625f406
KrisjanisP <krisjanispetrucena@gmail.com>
2026-02-10 16:14:53 +0200

    split throughput off of pairs.csv

--- merge.py
+++ merge.py
@@ -1,9 +1,17 @@
 import polars as pl
 
-GRAPH = "secoqc"
-files = [f"{GRAPH}_{walk}_hops.csv" for walk in ["r", "nb", "lrv"]]
-l = pl.read_csv(files[0])
-for r in files[1:]:
-    l = l.join(pl.read_csv(r), on=["source", "target"], how="inner", validate="1:1")
-    assert l.shape[0] == r.shape[0]
-l.write_csv(f"data/{GRAPH}/hops.csv")
+GRAPH = "nsfnet"
+# files = [f"out2/{GRAPH}_{walk}_hops.csv" for walk in ["r", "nb", "lrv"]]
+# l = pl.read_csv(files[0])
+# for r_file in files[1:]:
+#     r = pl.read_csv(r_file)
+#     l = l.join(r, on=["source", "target"], how="inner", validate="1:1")
+#     assert l.shape[0] == r.shape[0]
+# l.write_csv(f"data/{GRAPH}/hops.csv")
+l_path  = pl.read_csv(f"data/{GRAPH}/pairs.csv")
+r_path = pl.read_csv(f"data/{GRAPH}/hops.csv")
+l = pl.read_csv(l_path)
+r = pl.read_csv(r_path).select(["source", "target", "lrv_mean_hops"])
+l = l.join(r, on=["source", "target"], how="inner", validate="1:1")
+assert l.shape[0] == r.shape[0]
+l.write_csv(f"data/{GRAPH}/pairs.csv")
