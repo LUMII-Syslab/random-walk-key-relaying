c92177de249f0a39b8703ab2491289113e1d30b1
KrisjanisP <krisjanispetrucena@gmail.com>
2026-02-12 17:31:50 +0200

    add efficiencies and inflation

--- correlation.py
+++ correlation.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 
 GRAPH = "geant"
 data = pl.read_csv(f"data/{GRAPH}/pairs.csv")
-data = data.select(["lrv_hops", "lrv_efficiency"])
+data = data.select(["lrv_inflation", "lrv_nc_eff"])
+data = data.filter(pl.col("lrv_nc_eff") >= 0)
 
-plt.scatter(data["lrv_hops"], data["lrv_efficiency"])
-plt.xlabel("Hop Count")
+plt.scatter(data["lrv_inflation"], data["lrv_nc_eff"])
+plt.xlabel("Inflation")
 plt.ylabel("Efficiency")
-plt.title(f"Efficiency vs Hop Count for {GRAPH}")
+plt.title(f"Efficiency vs Inflation for {GRAPH}")
 plt.show()
