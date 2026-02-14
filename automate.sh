49993330ff0a0c863690f356929b317f2f983257
krisjanisp <krisjanispetrucena@gmail.com>
2026-02-14 11:01:16 +0200

    run in parallel

--- automate.sh
+++ automate.sh
@@ -2,15 +2,20 @@
 
 for NODES in $(seq 3 3 99); do
     echo "processing $NODES nodes..."
+    python3 construct.py graphs/generated/edges.csv "$NODES"
+
     for WALK in R NB LRV; do
-        OUT_DIR="./out/$NODES/$WALK"
-        mkdir -p "$OUT_DIR.tmp"
-        python3 construct.py graphs/generated/edges.csv "$NODES"
-        make run EDGE=constructed.csv WALK=$WALK OUT_DIR="$OUT_DIR.tmp"
-        python3 connectivity.py constructed.csv "$OUT_DIR.tmp"
-        mkdir -p "$OUT_DIR"
-        mv "$OUT_DIR.tmp"/* "$OUT_DIR/"
-        rm -rf "$OUT_DIR.tmp"
-        rm constructed.csv
+        (
+            OUT_DIR="./out/$NODES/$WALK"
+            mkdir -p "$OUT_DIR.tmp"
+            make run EDGE=constructed.csv WALK=$WALK OUT_DIR="$OUT_DIR.tmp"
+            python3 connectivity.py constructed.csv "$OUT_DIR.tmp"
+            mkdir -p "$OUT_DIR"
+            mv "$OUT_DIR.tmp"/* "$OUT_DIR/"
+            rm -rf "$OUT_DIR.tmp"
+        ) &
     done
+
+    wait
+    rm -f constructed.csv
 done
