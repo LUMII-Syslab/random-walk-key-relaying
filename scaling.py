d7dbd177bf55cf28f1c95c35955f7804e49c6570
krisjanisp <krisjanispetrucena@gmail.com>
2026-02-15 16:18:55 +0200

    scaling of throughput, hop count in larger graphs

--- scaling.py
+++ scaling.py
@@ -7,13 +7,32 @@ import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 x = list(range(6, 100, 3))
+walks = ["lrv", "nb", "r"]
+
 
 def main():
-    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(11, 4))
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, figsize=(11, 8))
     plot_throughput_avg(ax1)
     plot_hopcount_avg_and_median(ax2)
-    plt.tight_layout()
+    plot_efficiency(ax3)
+    plot_exposure(ax4)
+    plt.tight_layout(pad=4)
     plt.show()
+
+walk_styles = {
+    "lrv": {
+        "marker": "^",
+        "linestyle": "-",
+    },
+    "nb": {
+        "marker": "s",
+        "linestyle": "-",
+    },
+    "r": {
+        "marker": "o",
+        "linestyle": "-",
+    },
+}
 
 def avg_tput(x: int) -> dict[str, float]:
     res = dict()
@@ -25,20 +44,26 @@ def avg_tput(x: int) -> dict[str, float]:
     res["r"] = r_tput_csv["r_throughput"].mean()
     return res
 
-def plot_throughput_avg(ax: plt.Axes):
-    y_lrv = [avg_tput(x_i)["lrv"] for x_i in x]
-    y_nb = [avg_tput(x_i)["nb"] for x_i in x]
-    y_r = [avg_tput(x_i)["r"] for x_i in x]
-    ax.plot(x, y_lrv, marker='^', markersize=4, linestyle='-', label="LRV")
-    ax.plot(x, y_nb, marker='s', markersize=4, linestyle='--', label="NB")
-    ax.plot(x, y_r, marker='o', markersize=4, linestyle=':', label="R")
-    ax.legend(fontsize=12)
+def mean_metric(x: int, walk: str, file_name: str, col_name: str) -> float:
+    csv = pl.read_csv(f"out/{x}/{walk.upper()}/{file_name}")
+    return csv[col_name].mean()
+
+def node_count_ticks(ax: plt.Axes):
     ax.set_xlabel(translate.get_axis_label("node_count"),fontsize=12)
-    ax.set_ylabel(translate.get_axis_label("avg_tput"),fontsize=12)
     ax.set_xticks(x, labels=[str(x_i) if i%6==0 else "" for i, x_i in enumerate(x)], fontsize=12)
+
+def plot_throughput_avg(ax: plt.Axes):
+    for walk in walks:
+        y = [mean_metric(x_i, walk, "throughput.csv", f"{walk}_throughput") for x_i in x]
+        ax.plot(x, y, **walk_styles[walk], markersize=4, label=walk.upper())
+    ax.legend(fontsize=12)
+    node_count_ticks(ax)
+    ax.set_ylabel(f"{translate.get_axis_label("tput")} avg", fontsize=12)
     yticks = np.arange(0,2.5,0.5)
     ax.set_yticks(yticks, labels=[f"{y:.1f}" for y in yticks], fontsize=12)
     ax.yaxis.grid(True, alpha=0.5)
+    ax.set_title("Throughput vs network size", fontsize=14)
+    ax.set_ylim(0,2.5)
 
 def avg_hopcount(x: int) -> dict[str, float]:
     res = dict()
@@ -55,16 +80,61 @@ def median_hopcount(x: int) -> float:
     return hop_csv["lrv_q2_hops"].mean()
 
 def plot_hopcount_avg_and_median(ax: plt.Axes):
-    y_lrv = [avg_hopcount(x_i)["lrv"] for x_i in x]
-    y_nb = [avg_hopcount(x_i)["nb"] for x_i in x]
-    y_r = [avg_hopcount(x_i)["r"] for x_i in x]
-    # y2 = [median_hopcount(x_i) for x_i in x]
-    ax.plot(x, y_lrv, marker='^', markersize=4, linestyle='-', label="LRV")
-    ax.plot(x, y_nb, marker='s', markersize=4, linestyle='--', label="NB")
-    ax.plot(x, y_r, marker='o', markersize=4, linestyle=':', label="R")
+    for walk in walks:
+        y = [mean_metric(x_i, walk, "hops.csv", f"{walk}_mean_hops") for x_i in x]
+        ax.plot(x, y, **walk_styles[walk], markersize=4, label=walk.upper())
     ax.legend(fontsize=12)
-    ax.set_xlabel(translate.get_axis_label("node_count"),fontsize=12)
-    ax.set_ylabel(translate.get_axis_label("avg_hops"),fontsize=12)
+    node_count_ticks(ax)
+    ax.set_ylabel(f"{translate.get_axis_label('hops')} avg",fontsize=12)
+    ax.yaxis.grid(True, alpha=0.5)
+    ax.set_title("Hop count vs network size", fontsize=14)
+    ax.set_ylim(0,100)
+    ax.tick_params(axis="y", labelsize=12)
+
+def nc_efficiency(tput: float, exposure: float, conn: int) -> float:
+    return tput * (1 - exposure) / (conn - 1)
+
+def join_col(lhs: pl.DataFrame, rhs: pl.DataFrame, col: str) -> pl.DataFrame:
+    return lhs.join(rhs.select(["source","target",col]), on=["source","target"], how="inner", validate="1:1")
+
+def avg_efficiency(x: int) -> dict[str, float]:
+    res = dict()
+    for walk in walks:
+        tput_csv = pl.read_csv(f"out/{x}/{walk.upper()}/throughput.csv")
+        conn_csv = pl.read_csv(f"out/{x}/{walk.upper()}/connectivity.csv")
+        exp_csv = pl.read_csv(f"out/{x}/{walk.upper()}/exposure.csv")
+        params = tput_csv.select(["source","target"])
+        params = join_col(params, tput_csv, f"{walk}_throughput")
+        params = join_col(params, exp_csv, f"{walk}_max_vis_prob")
+        params = join_col(params, conn_csv, "connectivity")
+        params = params.drop(["source","target"])
+        nc_eff = [nc_efficiency(*row) for row in params.rows()]
+        res[walk] = np.mean(nc_eff)
+    return res
+
+def plot_efficiency(ax: plt.Axes):
+    for walk in walks:
+        y = [avg_efficiency(x_i)[walk] for x_i in x]
+        ax.plot(x,y, **walk_styles[walk], markersize=4, label=walk.upper())
+    ax.legend(fontsize=12)
+    node_count_ticks(ax)
+    ax.set_ylabel(f"{translate.get_axis_label('nc_eff')} avg",fontsize=12)
+    ax.yaxis.grid(True, alpha=0.5)
+    ax.set_title(f"T-put efficiency vs network size",fontsize=14)
+    ax.set_ylim(0,1)
+    ax.tick_params(axis="y", labelsize=12)
+
+def plot_exposure(ax: plt.Axes):
+    for walk in walks:
+        y = [mean_metric(x_i, walk, "exposure.csv", f"{walk}_max_vis_prob") for x_i in x]
+        ax.plot(x, y, **walk_styles[walk], markersize=4, label=walk.upper())
+    ax.legend(fontsize=12)
+    node_count_ticks(ax)
+    ax.set_ylabel(translate.get_axis_label("avg_max_vis_prob"), fontsize=12)
+    ax.yaxis.grid(True, alpha=0.5)
+    ax.set_title("Max visit probability vs network size", fontsize=14)
+    ax.set_ylim(0.5,1)
+    ax.tick_params(axis="y", labelsize=12)
 
 if __name__ == "__main__":
     main()
--- translate.py
+++ translate.py
@@ -1,34 +1,42 @@
 translations = {
     "lrv_nc_eff": "$\\eta$",
     "lrv_tput": "$\\overline{T}$",
+    "tput": "$\\overline{T}$",
     "shortest": "$d$",
     "lrv_max_vis_prob": "$X$",
     "lrv_max_vis_node": "$X_v$",
     "node_conn": "$C$",
     "lrv_hops": "$\\overline{H}$",
+    "hops": "$\\overline{H}$",
     "source": "$s$",
     "target": "$t$",
     "lrv_q2_hops": "$\\widetilde{H}$",
     "lrv_inflation": "$\\phi$",
     "lrv_infl_plus": "$\\phi^+$",
-    "node_count": "$n$",
+    "node_count": "$N$",
     "avg_tput": "$T$",
-    "avg_hops": "$H$"
+    "avg_hops": "$H$",
+    "nc_eff": "$\\eta$",
+    "avg_max_vis_prob": "$X$",
 }
 
 expansions = {
     "lrv_nc_eff": "secure throughput efficiency",
+    "nc_eff": "secure tput efficiency",
     "lrv_hops": "mean hop count",
     "lrv_q2_hops": "median hop count",
-    "lrv_tput": "estimated throughput",
+    "lrv_tput": "mean throughput",
+    "tput": "mean throughput",
+    "hops": "mean hop count",
     "lrv_inflation": "route inflation",
     "lrv_infl_plus": "route inflation via median",
     "node_count": "number of nodes",
-    "avg_tput": "throughput average",
-    "avg_hops": "hop count average",
+    "avg_tput": "throughput avg",
+    "avg_hops": "hop count avg",
+    "avg_max_vis_prob": "exposure avg",
 }
 
 def get_axis_label(key: str) -> str:
     if key not in expansions:
         return translations[key]
-    return f"{translations[key]} ({expansions[key]})"
+    return f"{translations[key]} ( {expansions[key]} )"
