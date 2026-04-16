93560bd117499648d04a77360afd0577a4120059
KrisjanisP <krisjanispetrucena@gmail.com>
2026-04-16 15:07:02 +0300

    continue scouted2 impl

--- cpp/scouted2.cpp
+++ cpp/scouted2.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <map>
+#include <memory>
 #include <queue>
+#include <random>
 #include <vector>
 #include "graph.hpp"
 #include "utils.hpp"
@@ -8,11 +11,19 @@
 using namespace std;
 
 const double SCOUTS_PER_SECONDS = 4;
+const double CLASSICAL_DELAY_MS = 5;
+const double QKD_SKR_BITS_P_S = 1000;
+const int CHUNK_SIZE_BITS = 256;
+
+mt19937 rng(2026);
 
 struct Options{
     string edges_csv = "";
     vector<string> src_nodes;
     bool verbose;
+    int watermark_sz = 32;
+    int ttl = 100;
+    int max_wait_time_s = 2;
 
     void print(){
         cout<<"edges_csv: "<<edges_csv<<endl;
@@ -24,43 +35,152 @@ enum class EventType {
     EmitScout,
     ScoutForward,
     ScoutReturn,
-    ChunkForward,
+    ChunkReceived,
 };
 
 struct Event{
     double time;
     EventType type;
-    
-    // emit scout event
-    int origin;
+
+    int origin; // emit scout + src node
+
+    // walk events
+    int sender;
+    int receiver;
+    shared_ptr<RwToken> token;
+
+    // full history in scout forward and planned path otherwise
+    vector<int> history;
+
+    int target=-1; // scout return
+    double wait; // wait before sending
 };
 
 bool operator<(const Event& lhs, const Event& rhs){
     return lhs.time < rhs.time;
 }
 
+bool consume(int keys_in_buff, int watermark, int hop_count, int ttl){
+    if(keys_in_buff>=watermark) return false;
+    assert(hop_count<=ttl);
+    double b = (double)keys_in_buff/(double)watermark;
+    double t = (double)(ttl-hop_count)/(double)ttl;
+    double p = 1 - b*t;
+    double r = (double)(rng()-rng.min())/(double)(rng.max()-rng.min());
+    return r <= p;
+}
+
+vector<int> erase_loops(vector<int> history){
+    vector<int> res;
+    map<int,size_t> lst_occ;
+    for(size_t i=0;i<history.size();i++)
+        lst_occ[history[i]]=i;
+    for(size_t i=0;i<history.size();i++){
+        int x = history[i];
+        while(i!=lst_occ[x]) i++;
+        res.push_back(x);
+    }
+    return res;
+}
+
 void run_simulation(const Options& opts, const Graph& graph){
     priority_queue<Event> pq;
-    
+
     for(string src: opts.src_nodes){
         int origin = graph.node_index(src);
-        pq.push(Event{0.0,EventType::EmitScout,origin});
+        pq.push(Event{0.0,EventType::EmitScout,origin,-1,-1,nullptr,{},{}});
     }
-    
+
+    map<pair<int,int>,int> established_keys;
+
+    map<pair<int,int>,int> reserved_total_on_qkd_link;
+
+    auto predict_wait_time = [&](double time_now, int from, int to) -> double{
+        if(from>to) swap(from,to);
+        double generated = QKD_SKR_BITS_P_S * time_now;
+        double extracted = reserved_total_on_qkd_link[{from,to}];
+        double req_bits_to_gen = 256-(generated-extracted);
+        double time_to_gen = req_bits_to_gen/QKD_SKR_BITS_P_S;
+        return max(time_to_gen, 0.0);
+    };
+
+    auto enqueue_on_link = [&](double time_now, int from, int to) -> double{
+        if(from>to) swap(from,to);
+        double generated = QKD_SKR_BITS_P_S * time_now;
+        double extracted = reserved_total_on_qkd_link[{from,to}];
+        reserved_total_on_qkd_link[{from,to}]++;
+        double req_bits_to_gen = 256-(generated-extracted);
+        double time_to_gen = req_bits_to_gen/QKD_SKR_BITS_P_S;
+        return max(time_to_gen, 0.0);
+    };
+
     while(pq.size()>0){
         Event e = pq.top();
         pq.pop();
-        
+
         if(e.type == EventType::EmitScout){
             double next_occurrence = e.time + 1/SCOUTS_PER_SECONDS;
-            pq.push(Event{next_occurrence,EventType::EmitScout,e.origin});
-            
-            // choose random neighbor
-            // create a new random walk token
-            
+            pq.push(Event{next_occurrence,EventType::EmitScout,e.origin,-1,-1,nullptr,{},-1});
+            shared_ptr<RwToken> token = make_shared<HsToken>(e.origin, -1, rng());
+            int ngh = token->choose_next_and_update(e.origin, graph.neighbors(e.origin));
+            double arrives_at = e.time + CLASSICAL_DELAY_MS/1000.0;
+            bool wait_time_ok = predict_wait_time(e.time, e.sender, e.receiver)<=opts.max_wait_time_s;
+            bool ttl_ok = 1 <= opts.ttl;
+            if(wait_time_ok&&ttl_ok)
+                pq.push(Event{arrives_at,EventType::ScoutForward,e.origin,e.origin,ngh,token,{e.origin,ngh}});
+            continue;
+        }
+
+        if(e.type == EventType::ScoutForward){
+            if(e.receiver!=e.origin&&consume(established_keys[{e.origin,e.receiver}], opts.watermark_sz, e.history.size()-1, opts.ttl)){
+                auto path = erase_loops(e.history);
+                double arrives_at = e.time + CLASSICAL_DELAY_MS/1000.0;
+                int nxt = path[path.size()-2];
+                double wait_time = enqueue_on_link(e.time, e.receiver, nxt);
+                pq.push(Event{arrives_at,EventType::ScoutReturn,e.origin,e.receiver,path[path.size()-2],nullptr,path,e.receiver,wait_time});
+                continue;
+            }
+            int ngh = e.token->choose_next_and_update(e.receiver, graph.neighbors(e.receiver));
+            vector<int> new_history = e.history;
+            new_history.push_back(ngh);
+            double arrives_at = e.time + CLASSICAL_DELAY_MS/1000.0;
+            bool wait_time_ok = predict_wait_time(e.time, e.sender, e.receiver)<=opts.max_wait_time_s;
+            bool ttl_ok = new_history.size()-1 <= opts.ttl;
+            if(wait_time_ok&&ttl_ok)
+                pq.push(Event{arrives_at,EventType::ScoutForward,e.origin,e.receiver,ngh,e.token,new_history});
+            continue;
+        }
+
+        if(e.type == EventType::ScoutReturn){
+            if(e.receiver == e.origin){
+                double chunk_at = e.time+e.wait+(e.history.size()-1)*CLASSICAL_DELAY_MS/1000.0;
+                pq.push(Event{chunk_at, EventType::ChunkReceived, e.origin, e.origin, e.target, nullptr, e.history, e.target, 0});
+                continue;
+            }
+            int idx=-1;
+            for(size_t i=0;i<e.history.size();i++){
+                if(e.history[i] == e.receiver) {idx=i;break;}
+            }
+            assert(idx>=1);
+            int nxt = e.history[idx-1];
+            double wait_time = max(e.wait,enqueue_on_link(e.time, e.receiver, nxt));
+            double arrives_at = e.time + CLASSICAL_DELAY_MS/1000.0;
+            pq.push(Event{arrives_at,EventType::ScoutReturn,e.origin,e.receiver,nxt,nullptr,e.history,e.target,wait_time});
             continue;
         }
         
+        if(e.type == EventType::ChunkReceived){
+            established_keys[{e.origin, e.target}]++;
+            if(opts.verbose){
+                cout<<"chunk";
+                for(int x: e.history){
+                    string name = graph.node_name(x);
+                    cout<<" "<<x;
+                }
+                cout<<endl;
+            }
+        }
+
     }
 }
 
@@ -71,7 +191,7 @@ int main(int argc, char* argv[]) {
     Graph graph = Graph(opts.edges_csv);
 
     opts.print();
-    
+
     run_simulation(opts, graph);
 }
 
@@ -96,12 +216,12 @@ Options parse_args(int argc, char* argv[]){
     for(int i=1;i<argc;i++){
         string flag = argv[i];
         seen_flags.insert(flag);
-        
+
         auto read_value = [&]() -> string{
             if(i+1>=argc) fail("flag "+flag+" requires a value");
             return argv[++i];
         };
-        
+
         if(flag=="-S"){ // source node list
             string src_node_list=read_value();
             opts.src_nodes = split(src_node_list,",");
