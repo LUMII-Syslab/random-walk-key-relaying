307ad425fdd66fc73c382a94ef64ec3a5cd68b7e
KrisjanisP <krisjanispetrucena@gmail.com>
2026-04-22 19:35:31 +0300

    dont tee by default and dont include halt at arg

--- run.sh
+++ run.sh
@@ -4,15 +4,12 @@ set -ex
 
 cd cpp
 make ./build/scouted
-RUN_NAME="scouted-geant-1-max-b-t"
 
 ./build/scouted \
     -S MAR \
     -e ../graphs/geant/edges.csv \
     --verbose \
-    --halt-at-keys 128 \
     --v-conn-cartel-size \
     --v-conn-csv ../graphs/geant/conn.csv \
     --block-chunks 32 \
-    | tee ../data/${RUN_NAME}.log | grep Halted
 
